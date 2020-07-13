1506b047bef94b6e314c0c9c687e8099a926940a
jack333 <gaius.july@gmail.com>
2020-07-13 11:59:36 +0900

    .

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-// uuid: Sun Jul 12 2020 22:05:42 GMT+0900 (GMT+09:00)
+// uuid: Mon Jul 13 2020 11:58:56 GMT+0900 (GMT+09:00)
 /* eslint-env serviceworker */
 /* eslint-disable no-restricted-globals */
 /* global workbox */
